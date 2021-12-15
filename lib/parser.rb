5e0043879ac681184de2de3605a0a88d6a772ce8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2021-12-15 13:30:57 +0100

    fix parsing " " word

--- lib/parser.rb
+++ lib/parser.rb
@@ -32,7 +32,7 @@ module Rpl
             opened_programs += 1
             elt.gsub!( '«', '« ') if elt.length > 1 && elt[1] != ' '
           end
-          string_delimiters += 1 if elt[0] == '"'
+          string_delimiters += 1 if elt[0] == '"' && elt.length > 1
 
           elt = "#{regrouped_input.pop} #{elt}".strip if regrouping
 
@@ -42,7 +42,7 @@ module Rpl
             closed_programs += 1
             elt.gsub!( '»', ' »') if elt.length > 1 && elt[-2] != ' '
           end
-          string_delimiters += 1 if elt.length > 1 && elt[-1] == '"'
+          string_delimiters += 1 if elt[-1] == '"'
 
           regrouping = string_delimiters.odd? || (opened_programs > closed_programs )
         end
