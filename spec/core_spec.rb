7d1a91705be9897c13b71be79f76d4abdb79c0ac
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2022-02-09 16:35:11 +0100

    indentation

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -8,16 +8,16 @@ require_relative '../lib/core'
 class TestParser < Test::Unit::TestCase
   def test_stack_extract
     stack, args = Rpl::Lang.stack_extract [{ value: 1, type: :numeric },
-                                                 { value: 2, type: :numeric }],
-                                                [:any]
+                                           { value: 2, type: :numeric }],
+                                          [:any]
     assert_equal [{ value: 1, type: :numeric }],
                  stack
     assert_equal [{ value: 2, type: :numeric }],
                  args
 
     stack, args = Rpl::Lang.stack_extract [{ value: 'test', type: :string },
-                                                 { value: 2, type: :numeric }],
-                                                [[:numeric], :any]
+                                           { value: 2, type: :numeric }],
+                                          [[:numeric], :any]
     assert_equal [],
                  stack
     assert_equal [{ value: 2, type: :numeric },
