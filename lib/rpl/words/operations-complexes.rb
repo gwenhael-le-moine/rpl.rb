d3d24cb1455024f550becd1a7406eafc627be8f7
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2022-08-31 12:21:46 +0200

    implement some Complex words

--- lib/rpl/words/operations-complexes.rb
+++ lib/rpl/words/operations-complexes.rb
@@ -9,48 +9,57 @@ module RplLang
         super
 
         category = 'Operations on complexes'
-        # Operations on complexes
-        # @dictionary.add_word( ['re'],
-        #                       category,
-        #                       '( c -- n ) complex real part',
-        #                       proc do
 
-        #                       end )
+        @dictionary.add_word( ['re'],
+                              category,
+                              '( c -- n ) complex real part',
+                              proc do
+                                args = stack_extract( [[RplComplex]] )
 
-        # @dictionary.add_word( 'im',
-        #                       category,
-        #                       '( c -- n ) complex imaginary part',
-        #                       proc do
+                                @stack << RplNumeric.new( args[0].value.real )
+                              end )
 
-        #                       end )
+        @dictionary.add_word( ['im'],
+                              category,
+                              '( c -- n ) complex imaginary part',
+                              proc do
+                                args = stack_extract( [[RplComplex]] )
 
-        # @dictionary.add_word( ['conj'],
-        #                       category,
-        #                       '( c -- c ) complex conjugate',
-        #                       proc do
+                                @stack << RplNumeric.new( args[0].value.imaginary )
+                              end )
 
-        #                       end )
+        @dictionary.add_word( ['conj'],
+                              category,
+                              '( c -- c ) complex conjugate',
+                              proc do
+                                args = stack_extract( [[RplComplex]] )
 
-        # @dictionary.add_word( 'arg',
-        #                       category,
-        #                       'complex argument in radians',
-        #                       proc do
+                                @stack << RplComplex.new( args[0].value.conjugate )
+                              end )
 
-        #                       end )
+        @dictionary.add_word( ['arg'],
+                              category,
+                              '( c -- n ) complex argument in radians',
+                              proc do
+                                args = stack_extract( [[RplComplex]] )
 
-        # @dictionary.add_word( ['c→r', 'c->r'],
-        #                       category,
-        #                       '( c -- n n ) transform a complex in 2 reals',
-        #                       proc do
+                                @stack << RplNumeric.new( args[0].value.arg )
+                              end )
 
-        #                       end )
+        @dictionary.add_word( ['c→r', 'c->r'],
+                              category,
+                              '( c -- n n ) transform a complex in 2 reals',
+                              Types.new_object( RplProgram, '« dup re swap im »' ) )
 
-        # @dictionary.add_word( ['r→c', 'r->c'],
-        #                       category,
-        #                       '( n n -- c ) transform 2 reals in a complex',
-        #                       proc do
+        @dictionary.add_word( ['r→c', 'r->c'],
+                              category,
+                              '( n n -- c ) transform 2 reals in a complex',
+                              proc do
+                                args = stack_extract( [[RplNumeric], [RplNumeric]] )
 
-        #                       end )
+                                complex_as_string = "(#{args[1].value}#{args[0].value.positive? ? '+' : ''}#{args[0].value}i)"
+                                @stack << RplComplex.new( complex_as_string )
+                              end )
 
         # @dictionary.add_word( ['p→r', 'p->r'],
         #                       category,
