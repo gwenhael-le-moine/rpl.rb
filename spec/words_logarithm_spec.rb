339b64486e79e7a264bc31bace09a9d543d976c9
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2022-10-04 14:12:48 +0200

    test logarithm words (only 'e' for the moment)

---
+++ spec/words_logarithm_spec.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+require 'rpl'
+require 'rpl/types'
+
+class TesttLanguageLogarithm < MiniTest::Test
+  include Types
+
+  def test_e
+    interpreter = Rpl.new
+    interpreter.run 'e'
+    assert_equal [Types.new_object( RplNumeric, BigMath.E( RplNumeric.precision ) )],
+                 interpreter.stack
+  end
+end
